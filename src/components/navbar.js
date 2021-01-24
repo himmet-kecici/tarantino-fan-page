e899f9f59d78ba306a9863b1035ad78e7aa21e12
himmet <kecicihimmet@gmail.com>
2021-01-25 02:07:17 +0300

    finished the project

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
     <nav>
-        <h2 className='logo'><a className='logo-link' href='/#'>Quentin Tarantino</a></h2>
+        <h2 className='logo'><a className='logo-link' href='/'>Quentin Tarantino</a></h2>
         <ul className='nav-menu'>
             <li><NavLink className="nav-menu__link" activeClassName='activate' exact to="/">Home</NavLink></li>
             <li><NavLink className="nav-menu__link" activeClassName='activate' to="/asdirectors/">As Director</NavLink></li>
