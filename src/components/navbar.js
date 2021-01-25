e0626d880bf5e51465bb825561fa8d1a39415b10
himmet <kecicihimmet@gmail.com>
2021-01-25 03:04:49 +0300

    deleted activate class

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
-    <nav>
-        <h2 className='logo'>Quentin Tarantino</h2>
+    <div className="nav">
+        <h2 className='logo'><NavLink className="logo-link" exact to="/">Quentin Tarantino</NavLink></h2>
         <ul className='nav-menu'>
 
-            <li><NavLink className="nav-menu__link" activeClassName='activate' exact to="/">Home</NavLink></li>
-            <li><NavLink className="nav-menu__link" activeClassName='activate' to="/asdirectors/">As Director</NavLink></li>
-            <li><NavLink className="nav-menu__link" activeClassName='activate' to="/asactors/">As Actor</NavLink></li>
+            <li><NavLink className="nav-menu__link" exact to="/">Home</NavLink></li>
+            <li><NavLink className="nav-menu__link" to="/asdirectors/">As Director</NavLink></li>
+            <li><NavLink className="nav-menu__link" to="/asactors/">As Actor</NavLink></li>
         </ul>
-    </nav>
+    </div>
 )
 export default Navbar;
